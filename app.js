f6d82471948ce27704b813ee22eb61d748dbc24a
Sanchit Chadha <schadha@vt.edu>
2014-03-23 18:26:31 -0400

    Got android integration going now

--- app.js
+++ app.js
@@ -54,10 +54,8 @@ app.post("/chat/join/:room", function (req, res) {
 });
 
 app.post("/chat/clients", function (req, res) {
-	var roomNames = req.body.rooms[0].split(",");
+	var roomNames = req.body.rooms.split(",");
 	var numClients = [];
-	console.log(roomNames.length);
-	console.log(roomNames[0]);
 	for (var i = 0; i < roomNames.length; i++) {
 		numClients[i] = app.io.sockets.clients(roomNames[i]).length;
 	}
